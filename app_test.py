615c93d98cc90a956f09208377176663ee74c033
Molly Machin <mm1121@imperial.ac.uk>
2024-10-21 15:32:13 +0100

    Testing

--- app_test.py
+++ app_test.py
@@ -2,8 +2,8 @@ from app import process_query
 
 
 def test_knows_about_dinosaurs():
-    assert process_query("dinosaurs") == """Dinosaurs ruled the Earth 200
-                                             million years ago"""
+    test_str = "Dinosaurs ruled the Earth 200 million years ago"
+    assert process_query("dinosaurs") == test_str
 
 
 def test_does_not_know_about_asteroids():
