45b8b83b80038c2ef3b1747a21197237cadfb410
Molly Machin <mm1121@imperial.ac.uk>
2024-10-21 14:28:47 +0100

    Fixed Flake8 styling

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
     input_save = request.form.get("save")
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-    return render_template("hello.html", name=input_name, age=input_age, save=input_save)
+    return render_template("hello.html", name=input_name,
+                           age=input_age, save=input_save)
