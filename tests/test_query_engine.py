833638269d01de459e0cd1ec72af26265a48700b
Mark O'Neill <aoneillmark@gmail.com>
2024-10-28 15:39:44 +0000

    remove other than prime

--- tests/test_query_engine.py
+++ tests/test_query_engine.py
@@ -7,27 +7,27 @@ prime_q1 = "Which of the following numbers"
 prime_q2 = "are primes: 10, 78, 70, 27, 17?"
 
 
-def test_query_about_dinosaurs_returns_historical_fact():
-    input = "Which of the following numbers is the largest: 31, 48, 7?"
-    assert process_query(input) == "48"
+# def test_query_about_dinosaurs_returns_historical_fact():
+#     input = "Which of the following numbers is the largest: 31, 48, 7?"
+#     assert process_query(input) == "48"
 
 
-def test_query_about_anything_that_isnt_dinosaurs_returns_empty_string():
-    assert process_query("What is 21 plus 67?") == "88"
+# def test_query_about_anything_that_isnt_dinosaurs_returns_empty_string():
+#     assert process_query("What is 21 plus 67?") == "88"
 
 
-def test_about_multiplying():
-    assert process_query(var) == "1407"
+# def test_about_multiplying():
+#     assert process_query(var) == "1407"
 
 
-def test_about_cubes():
-    assert process_query(
-        square_var + cube_var
-        ) == "64"
+# def test_about_cubes():
+#     assert process_query(
+#         square_var + cube_var
+#         ) == "64"
 
 
-def test_minus():
-    assert process_query("What is 10 minus 5?") == "5"
+# def test_minus():
+#     assert process_query("What is 10 minus 5?") == "5"
 
 
 def prime():
