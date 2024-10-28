fd8f84f024fdeb1e7fc668c28aaed56efdc2f871
Arneaj <arnaud.rollandmail@gmail.com>
2024-10-28 14:52:16 +0000

    aaaa

--- tests/test_query_engine.py
+++ tests/test_query_engine.py
@@ -2,7 +2,9 @@ from app import process_query
 
 
 def test_query_about_dinosaurs_returns_historical_fact():
-	assert process_query("Which of the following numbers is the largest: 31, 48, 7?") == "48"
+    input = "Which of the following numbers is the largest: 31, 48, 7?"
+    assert process_query(input) == "48"
+
 
 def test_query_about_anything_that_isnt_dinosaurs_returns_empty_string():
-	assert process_query("What is 21 plus 67?") == "88"
+    assert process_query("What is 21 plus 67?") == "88"
