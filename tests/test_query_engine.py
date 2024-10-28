f43e0f6eb13ec6d9f1c2ac2d46e21813b3aa0721
Mark O'Neill <aoneillmark@gmail.com>
2024-10-28 14:43:27 +0000

    Process tests for new queries

--- tests/test_query_engine.py
+++ tests/test_query_engine.py
@@ -1,9 +1,9 @@
 from app import process_query
 
 def test_query_about_dinosaurs_returns_historical_fact():
-	assert process_query("dinosaurs") == "Dinosaurs ruled the Earth 200 million years ago"
+	assert process_query("Which of the following numbers is the largest: 31, 48, 7?") == "48"
 
 def test_query_about_anything_that_isnt_dinosaurs_returns_empty_string():
-	assert process_query("imperial college") == "Unknown"
+	assert process_query("What is 21 plus 67?") == "88"
 
 
